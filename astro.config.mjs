2cd76924c5e5c765347803fd9d4c6ace66bd32e0
AD Gomez <garyeld12@gmail.com>
2026-01-01 18:13:05 -0400

    updating

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import alpinejs from '@astrojs/alpinejs';
+
 export default defineConfig({
   site: 'https://www.digitallevel.org', // Cambia esto por tu dominio real
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
 });
